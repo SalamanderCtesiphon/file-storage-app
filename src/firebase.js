fc8b16df09b86a1d7b91a1a94e47e7f52f015ad1
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-27 20:06:27 -0500

    updated to current method of initializing firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
+import firebase from "firebase/compat/app"
+import 'firebase/compat/auth'
 
 const app = firebase.initializeApp({
 
@@ -17,6 +17,6 @@ const app = firebase.initializeApp({
 
 })
 
-export const auth = app.auth()
+export const auth = firebase.auth()
 
 export default app
