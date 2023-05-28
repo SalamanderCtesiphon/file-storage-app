3277281570837d378e9a9671d4b2f3dc71c0bc00
salamanderctesiphon <swbrookshire@gmail.com>
2023-05-27 20:05:12 -0500

    the tutorial was using a deprecated method to intialize firebase I had to refactor the code in order to be in complicance with the modern syntax

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,19 @@
+import { Container } from "react-bootstrap";
+import SignUp from "./SignUp";
+import { AuthProvider } from "../contexts/AuthContext";
 
 function App() {
-  return "hello world!"
+  return (
+    <AuthProvider>
+      <Container className="d-flex align-items-center justify-content-center" 
+      style={{minHeight: "100vh"}}>
+        <div className="w-100" style={{maxWidth: "400px"}}>
+          <SignUp />
+        </div>
+      </Container>
+    </AuthProvider>
+    
+  )
 }
 
 export default App;
